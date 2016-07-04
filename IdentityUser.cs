e46143fab6953923471a8a155db04fd9cbeab721
Matthew Duggan <matthew@moretalk.uk.com>
2016-07-04 15:17:36 +0100

    Initial Code Commit

---
+++ IdentityUser.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+
+namespace AspNet.Identity.MySQL
+{
+    /// <summary>
+    /// Class that implements the ASP.NET Identity
+    /// IUser interface 
+    /// </summary>
+    public class IdentityUser : IUser
+    {
+        /// <summary>
+        /// Default constructor 
+        /// </summary>
+        public IdentityUser()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        /// Constructor that takes user name as argument
+        /// </summary>
+        /// <param name="userName"></param>
+        public IdentityUser(string userName)
+            : this()
+        {
+            UserName = userName;
+        }
+
+        /// <summary>
+        /// User ID
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// User's name
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// User's password hash
+        /// </summary>
+        public string PasswordHash { get; set; }
+
+        /// <summary>
+        /// User's security stamp
+        /// </summary>
+        public string SecurityStamp { get; set; }
+
+        public int ClientId { get; set; }
+
+        public int userid { get; set; }
+
+        public string pw_reset_key { get; set; }
+
+        public DateTime? pw_reset_expiry { get; set; }
+    }
+}
